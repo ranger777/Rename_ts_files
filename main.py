ebfb53caaa3ce5b7522269861c7314e2b9688e0e
ranger777 <ranger-777@mail.ru>
2024-03-04 02:32:32 +0700

    готов механизм создания нового имени

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import os
+from subprocess import check_output, CalledProcessError, STDOUT
+import subprocess32 as subprocess
+
+
 
 def rename():
     dir = os.path.abspath(os.curdir)#получим путь к папке откуда запускаем скрипт
     print(dir)
     #dir_list = sorted(os.listdir(dir), reverse=True)
 
-    #получить список всех папок
-    dir_list = sorted(os.listdir(dir), reverse=True)
-    print(dir_list)
-    #циклом пороходим и ищем все папки
-    for dir_el in dir_list:
-        print(f'Открываем папку {dir_el}')
-        dir_el_list = sorted(os.listdir(dir), reverse=True)#считываем содержимое
-        ps_count = dir_el_list.count('ps')
-        vs_count = dir_el_list.count('vs')
-        if ps_count > 0 and vs_count > 0:
-            pass#основной код
 
+    tree = os.walk(dir)# получим дерево каталогов
 
-        #если нет напишем что папка не подходит для начала основного скрипта
+    for i in tree:
+        if i[1] == ['ps', 'vs']: # если в папке есть и ps и vs, то это нужная папка
+            print(f'В папке {i[0]}:')
+            for file in i[2]: # пройдем по всем файлам в этой папке
+                if file.endswith('.ts'): # если расширение ts, то это нужный файл
+                    filename = f'{i[0]}/{file}' # соберем полный путь с именем файла
+                    #соберем строку для запроса в коммандную строку
+                    command = (f'"C:\\Users\\Ranger\\Downloads\\ffmpeg-2024-02-29-git-4a134eb14a-full_build\\ffmpeg-2024-02-29-git-4a134eb14a-full_build\\bin\\ffprobe.exe" -i {filename} -show_entries format=duration -v quiet -of csv="p=0"')
+                    p = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    text = p.stdout.read() # получим текстовое значение продолжительности видеофайла
+                    time_duration = format(float(text), ".3f")# преобразуем в число с 3 знакими после запятой
+                    #retcode = p.wait()
+                    print(f'{os.path.splitext(file)[0]} - {time_duration}')
+                    base_name = os.path.splitext(file)[0].partition('T')[0]
+                    print(f'{base_name}T{time_duration}\n')
+
+        #Доработать бэкап файла. лучше заменой расширения
 
 
 
